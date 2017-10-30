502f625e1d10a165ea3b04174dcd70f6c6fe5d1e
Verta-master <vertyagina@gmail.com>
2017-10-30 16:27:23 +0600

    Update 2017-10-30T10:26:55.178Z

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Mobile menu
+
 var navMain = document.querySelector(".menu");
 var navToggle = document.querySelector(".menu__btn");
 
@@ -10,3 +12,44 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("menu--opened");
   }
 });
+
+//Sidebar navigation
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('sidebar__link--active');
+        })
+        $(this).addClass('sidebar__link--active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.sidebar__item a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.sidebar__item a').removeClass("sidebar__link--active");
+            currLink.addClass("sidebar__link--active");
+        }
+        else{
+//            currLink.removeClass("sidebar__link--active");
+        }
+    });
+}
